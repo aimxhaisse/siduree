9b222c1422aad5781590977ab0134441029547e4
s. rannou <mxs@sbrk.org>
2014-05-25 20:57:37 +0200

    [mockup] added jquery waypoints.

--- mockup/js/siduree.js
+++ mockup/js/siduree.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
     var slides = $('.slide');
+    var current_slide = parseInt(slides.first().attr('id').split('-')[1]);
+
     slides.each(function () {
 	var slide = $(this);
 	slide.find('.photo-background').each(function () {
 	    var photo = $(this);
 	    photo.attr({
 		'data-center': 'background-position: 50% 0px;',
-		'data-top-bottom': 'background-position: 50% -200px;',
+		'data-top-bottom': 'background-position: 50% -500px;',
+		'data-bottom-top': 'background-position: 50% 500px;',
 		'data-anchor-target': '#' + slide.attr('id')
 	    });
-	    var content = photo.find('.photo-content');
-	    content.attr({
-		'data-center': 'bottom: 200px; opacity: 1',
-		'data-top': 'bottom: 1200px; opacity: 0.5'
+
+	    var title = photo.find('.photo-content');
+	    title.attr({
+		'data-bottom': 'top: 5%; opacity: 0',
+		'data-center': 'top: 10%; opacity: 1',
 	    });
+
 	    var img_url = slide.attr('data-photo');
 	    photo.css({
 		'background-image': 'url(' + img_url + ')',
@@ -27,9 +32,51 @@ $(document).ready(function() {
 	});
 	slide.height($(window).height());
     });
+    
+    slides.waypoint(function (direction) {
+	if (direction == "down") {
+	    current_slide = $(this).index();
+	}
+    }, { offset: '50%' }).waypoint(function (direction) {
+	if (direction == "up") {
+	    current_slide = $(this).index();
+	}
+    }, { offset: '-50%'});
 
     var skr = skrollr.init({
 	forceHeight: false
     });
     skr.refresh(slides);
-}); 
+
+    var is_sliding = false;
+
+    var scrollTo = function (where) {
+	if (!is_sliding) {
+	    $('#slide-' + where).each(function () {
+		is_sliding = true;
+		$("html, body").animate({
+		    scrollTop: ($(this).offset().top - 50 /* navbar's height */) + "px"
+		}, 1500, function () {
+		    is_sliding = false;
+		});
+	    });
+	}
+    };
+
+    $('#go-up').click(function() {
+	scrollTo(current_slide - 1);
+	return false;
+    });
+
+    $('#go-down').click(function() {
+	scrollTo(current_slide + 1);
+	return false;
+    });
+
+    $(window).keypress(function(e) {
+	if (e.keyCode == 0 /* space bar */) {
+	    scrollTo(current_slide + 1);
+	}
+    });
+
+});
