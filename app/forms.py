b121365ccb8f934270e86d2de71b6e26157caf72
s. rannou <mxs@sbrk.org>
2014-05-27 22:02:41 +0200

    [app] login/register forms.

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from flask.ext.wtf import Form
+from wtforms import TextField
+from wtforms import PasswordField
+from wtforms.validators import Required
+from wtforms.validators import EqualTo
+
+class LoginForm(Form):
+    login = TextField('Login', [Required()])
+    password = PasswordField('Password', [Required(), EqualTo('again', message='Passwords must match')])
+
+class RegisterForm(Form):
+    login = TextField('Login', [Required()])
+    password = PasswordField('Password', [Required(), EqualTo('again', message='Passwords must match')])
+    again = PasswordField('Repeat Password')
--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
 from app import app
 from flask import render_template
+from forms import LoginForm
+from forms import RegisterForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title = 'Welcome!')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html', form = LoginForm(), title = 'Sign In')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    return render_template('register.html', form = RegisterForm(), title = 'Register')
