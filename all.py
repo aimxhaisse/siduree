4f081182043e7c9a7a0ecc867a94d81bd2f11a1b
s. rannou <mxs@sbrk.org>
2014-07-02 00:21:54 +0200

    [app] added fixtures with real photos from Italy.

--- all.py
+++ all.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
+# -*- coding: utf-8 -*-
 
 import argparse
 import subprocess
@@ -30,6 +31,7 @@ def populate():
     db.session.add(u)
     db.session.commit()
 
+    """
     for i in range(3):
         j = models.Journey()
         j.create(random_sentence(), random_sentence(), u.id)
@@ -47,6 +49,52 @@ def populate():
                 p.create(random_sentence(), random_sentence(), s.id, 'assets/lost.jpg')
                 db.session.add(p)
                 db.session.commit()
+    """
+
+    j = models.Journey()
+    j.create(u"Italy, Juin 2014", u"Trek de 6 jours dans les Alpes Italiennes au sein du parc national du Gran Paradiso", u.id)
+    db.session.add(j)
+    db.session.commit()
+
+    s = models.Slide()
+    s.create(u"Colle Del Nivolet, 2616m", u"21 Juin 2014", j.id)
+    db.session.add(s)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Vallone di Seiva", "", s.id, 'assets/italy/day_01/IMG_4257.jpg')
+    db.session.add(p)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Vallone di Nivolet", "", s.id, 'assets/italy/day_01/IMG_4263.jpg')
+    db.session.add(p)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Colle del Nivolet", "", s.id, 'assets/italy/day_01/IMG_4275.jpg')
+    db.session.add(p)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Colle del Nivolet", "", s.id, 'assets/italy/day_01/IMG_4283.jpg')
+    db.session.add(p)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Colle del Nivolet", "", s.id, 'assets/italy/day_01/IMG_4284.jpg')
+    db.session.add(p)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Colle del Nivolet", "", s.id, 'assets/italy/day_01/IMG_4286.jpg')
+    db.session.add(p)
+    db.session.commit()
+
+    p = models.Photo()
+    p.create(u"Colle del Nivolet", "", s.id, 'assets/italy/day_01/IMG_4290.jpg')
+    db.session.add(p)
+    db.session.commit()
 
 def main(what):
     if what == "dev-init":
@@ -54,6 +102,6 @@ def main(what):
         subprocess.call("mkdir -p app/static/uploads", shell = True)
         db.create_all()
         populate()
-        
+
 if __name__ == '__main__':
     main(args.command)
